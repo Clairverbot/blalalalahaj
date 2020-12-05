cc5d19e60b38f075362934044135083bceca9c36
claier <claire.xw205@gmail.com>
2020-12-05 19:55:14 +0800

    shark jump

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { Component } from "react";
 import MIDISounds from "midi-sounds-react";
 import "./App.css";
 import blahaj_percussion from "./assets/blahaj-percussion.png";
+import blahaj_percussion_right from "./assets/blahaj-percussion-right.png";
 import blahaj_ooh from "./assets/blahaj-ooh.png";
+import blahaj_ooh_right from "./assets/blahaj-ooh-right.png";
 
 class App extends Component {
   constructor(props) {
@@ -13,7 +15,6 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    this.envelopes = [];
     this.startListening();
     document.addEventListener("keydown", this.handleKeyDown.bind(this));
   }
@@ -28,20 +29,33 @@ class App extends Component {
         [this.calculateNote(parseInt(String.fromCharCode(e.keyCode)))],
         0.3
       );
+      this.animateShark("shark-ooh", 3);
     } else if (e.keyCode === 48) {
       this.midiSounds.playChordNow(598, [42], 0.3);
+      this.animateShark("shark-ooh", 3);
     } else if (e.keyCode >= 65 && e.keyCode <= 90) {
       let char_arr = "qwertyuiopasdfghjklzxcvbnm".split("");
-      let pos = char_arr.indexOf(String.fromCharCode(e.keyCode).toLowerCase())+11;
-      console.log(String.fromCharCode(e.keyCode));
+      let pos =
+        char_arr.indexOf(String.fromCharCode(e.keyCode).toLowerCase()) + 11;
       this.midiSounds.playChordNow(598, [this.calculateNote(pos)], 0.3);
+      this.animateShark("shark-ooh", 3);
+    } else if (e.keyCode === 32) {
+      this.midiSounds.playChordNow(1235, [70], 0.3);
+      this.animateShark("shark-percussion", 1);
     }
   }
   calculateNote(num) {
-    num=num-1
+    num = num - 1;
     let arr = [0, 2, 4, 5, 7, 9, 11];
-    console.log(num)
-    return 12* (Math.floor(num / 7)+0.5) + arr[num % 7];
+    return 12 * (Math.floor(num / 7) + 0.5) + arr[num % 7];
+  }
+  animateShark(className, sharkNum) {
+    const shark = document.getElementsByClassName(className);
+    const i = Math.floor(Math.random() * (sharkNum + 1));
+    shark.item(i).classList.add("jump");
+    setTimeout(() => {
+      shark.item(i).classList.remove("jump");
+    }, 1000);
   }
   onMIDIOnStateChange(event) {
     this.setState({
@@ -86,26 +100,27 @@ class App extends Component {
     return (
       <div className="App">
         <div className="shark-container">
-          <img src={blahaj_percussion} alt="percussion" />
-          <img src={blahaj_ooh} alt="ooh" />
-          <img src={blahaj_ooh} alt="ooh" />
-          <img src={blahaj_ooh} alt="ooh" className="shark-right" />
-          <img src={blahaj_ooh} alt="ooh" className="shark-right" />
           <img
             src={blahaj_percussion}
             alt="percussion"
-            className="shark-right"
+            className="shark-percussion"
+          />
+          <img src={blahaj_ooh} alt="ooh" className="shark-ooh" />
+          <img src={blahaj_ooh} alt="ooh" className="shark-ooh" />
+          <img src={blahaj_ooh_right} alt="ooh" className="shark-ooh" />
+          <img src={blahaj_ooh_right} alt="ooh" className="shark-ooh" />
+          <img
+            src={blahaj_percussion_right}
+            alt="percussion"
+            className="shark-percussion"
           />
         </div>
         <footer>
-          Created for SharkHacks 2020 🦈🎶
-          Powered by
+          Created for SharkHacks 2020 🦈🎶 Powered by
           <MIDISounds
-            className="midi-sound"
             ref={(ref) => (this.midiSounds = ref)}
             appElementName="root"
-            instruments={[598]}
-            drums={[2, 33]}
+            instruments={[598, 1235]}
           />
         </footer>
       </div>
